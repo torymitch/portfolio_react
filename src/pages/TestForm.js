18b64ffc7f3d8cb9a91895ec428aee6d19b8639b
Tory Mitchell <torymitch@gmail.com>
2024-07-30 22:24:17 -0400

    More Test

--- src/pages/TestForm.js
+++ src/pages/TestForm.js
@@ -8,9 +8,27 @@ export default function TestForm() {
     const toggleForm = () => {
         setShowForm(!showForm);
     };
+
+    const [input1, setInput1] = useState('');
+
+    const handleInput1Change = (event) => {
+        setInput1(event.target.value);
+    };
+
+    const [input2, setInput2] = useState('');
+
+    const handleInput2Change = (event) => {
+        setInput2(event.target.value);
+    };
+
+    const [isChecked, setIsChecked] = useState(false);
+
+   const handleCheckboxChange = (event) => {
+       setIsChecked(event.target.checked);
+   };
     
     const submitForm = () => {
-        alert(`Submit the form!`);
+        alert(`Input1 Is ${input1} && Input2 Is ${input2} && Checked is ${isChecked}`);
     };
 
       if(showForm) {
@@ -33,21 +51,25 @@ export default function TestForm() {
             <div className="modal">
                 <div onClick={toggleForm} className="overlay"></div>
                 <div className="modal-content">
-                <h2>Hello Form</h2>
+                <h2>Populate The Form</h2>
                 <p>
                     {/* This is a test of some modal */}
                     <form action={search}>
                         <div> 
                             <label>
-                                Input 1: <input name="input1" />
+                                Input 1: <input name="input1" type="text" value={input1} onChange={handleInput1Change}/>
                             </label>
                         </div>
                         <div> 
                             <label>
-                                Input 2: <input name="input2" />
+                                Input 2: <input name="input2" type="text" value={input2} onChange={handleInput2Change}/>
                             </label>
                         </div>
-                        
+                        <div>
+                            <label>
+                                Checkbox: <input type="checkbox" value={isChecked} onChange={handleCheckboxChange}/>
+                            </label>
+                        </div>
                         <div>
                             <button className="submit-form" onClick={submitForm}>SUBMIT</button>
                         </div>
