3125e7771e0dc23041ae5cf02af8a5e164bc71ef
Tory Mitchell <torymitch@gmail.com>
2024-07-31 13:20:16 -0400

    Added Redux Store

--- src/ReactRouter.js
+++ src/ReactRouter.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Note } from "./pages/Note";
 import { NotesPage } from "./pages/NotesPage";
 import { NotesDetailPage } from "./pages/NotesDetailPage";
+import { TestStore } from "./pages/TestStore";
 import { NotFoundPage } from "./pages/NotFoundPage";
 
 export const ReactRouter = () => {
@@ -12,6 +13,7 @@ export const ReactRouter = () => {
                     <Route path="/" element={<NotesPage />} exact />
                     <Route path="/note/:id" element={<Note />} />
                     <Route path="/notes" element={<NotesDetailPage />} />
+                    <Route path="/store" element={<TestStore />} />
                     <Route path="*" element={<NotFoundPage />} />
                 </Routes>   
             </Router>
---
+++ src/pages/TestStore.js
@@ -0,0 +1,50 @@
+// import { CreateStore } from 'redux';
+// import { Provider, connect, useSelector, dispatch } from "react-redux";
+ 
+export const TestStore = () => {
+    // const countReducer = function(state = 0, action) {
+    // switch (action.type) {
+    //     case "ADD":
+    //     return state + 1;
+    //     case "SUBTRACT":
+    //     return state + 1;
+    //     default:
+    //     return state;
+    // }
+    // }
+
+    // const store = CreateStore(countReducer)
+
+    // const mapStateToProps = state => {
+    // return {
+    //     const: state
+    // }
+    // }
+
+    // const mapDispatchToProps = state => {
+    // return {
+    //     add: () => dispatch({type: "ADD"}),
+    //     subtract: () => dispatch({type: "SUBTRACT"})
+
+    // }
+    // }
+
+    // const Component = ({count}) => {
+    // return (
+    //     <h1>Count = {count} </h1>
+    // )
+    // }
+
+    // const Container = connect(mapStateToProps, mapDispatchToProps)()
+
+return (
+    <>
+        <div>
+            Build It
+            {/* <Provider store = {store}>
+                <Container />
+            </Provider> */}
+        </div>
+    </>
+  )
+}
